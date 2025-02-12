621bd30dbd035c870cda0002d49bc453145a73bc
ebwood <timestamp.api@gmail.com>
2025-02-13 01:05:20 +0800

    book list

---
+++ components/BookCard.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import BookCover from "./BookCover";
+import Link from "next/link";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+
+const BookCard = ({
+  id,
+  title,
+  genre,
+  coverColor,
+  coverUrl,
+  isLoanedBook = false,
+}: Book) => {
+  return (
+    <li className={cn(isLoanedBook && "xs:w-52 w-full")}>
+      <Link
+        href={`/books/${id}`}
+        className={cn(isLoanedBook && "w-full flex flex-col items-center")}
+      >
+        <BookCover coverColor={coverColor} coverUrl={coverUrl} />
+
+        <div className={cn("mt-4", !isLoanedBook && "xs:max-w-40 max-w-28")}>
+          <p className="book-title">{title}</p>
+          <p className="book-genre">{genre}</p>
+        </div>
+
+        {isLoanedBook && (
+          <div className="mt-3 w-full">
+            <div className="book-loaned">
+              <Image
+                src="/icons/calendar.svg"
+                alt="calendar"
+                width={18}
+                height={18}
+                className="object-contain"
+              />
+              <p className="text-light-100">11 days left to return</p>
+            </div>
+
+            {/* 为什么book-btn里面的bg-dark-600没起作用呢 */}
+            <Button className="book-btn bg-dark-600">Download receipt</Button>
+          </div>
+        )}
+      </Link>
+    </li>
+  );
+};
+
+export default BookCard;
