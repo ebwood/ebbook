a23d21d398fe8eed349bcdb9ad7681f985427a4d
ebwood <timestamp.api@gmail.com>
2025-02-14 00:25:23 +0800

    neon + drizzle

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,23 @@
-import BookList from '@/components/BookList';
-import BookOverview from '@/components/BookOverview';
-import { sampleBooks } from '@/constants';
-import React from 'react';
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
+// import { db } from "@/database/drizzle";
+// import { users } from "@/database/schema";
+import React from "react";
 
-const Home = () => {
-  return (<>
-    <BookOverview {...sampleBooks[0]} />
-    <BookList title="Latest Books" books={sampleBooks} containerClassName="mt-28">
-
-    </BookList>
-  </>);
-
+const Home = async () => {
+  // const result = await db.select().from(users);
+  return (
+    <>
+      {/* <h1 className="text-white">{JSON.stringify(result)}</h1> */}
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      ></BookList>
+    </>
+  );
 };
 
 export default Home;
