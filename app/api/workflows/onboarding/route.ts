096344af361923f383531c8954c7ddc236ea40a4
ebwood <timestamp.api@gmail.com>
2025-02-14 23:17:14 +0800

    add resend email with upstash workflow

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -1,44 +1,73 @@
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { sendEmail } from "@/lib/workflow";
 import { serve } from "@upstash/workflow/nextjs";
+import { eq } from "drizzle-orm";
 
+type UserState = "non-active" | "active";
 type InitialData = {
   email: string;
+  fullName: string;
+};
+
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
+const THIRTY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
+
+const getUserState = async (email: string): Promise<UserState> => {
+  const user = await db.select().from(users).where(eq(users.email, email)).limit(1);
+  if (user.length === 0) {
+    return "non-active";
+  }
+
+  const lastActivityDate = new Date(user[0].lastActivityDate!);
+  const now = new Date();
+  const diffTime = now.getTime() - lastActivityDate.getTime();
+
+  if (diffTime > THREE_DAYS_IN_MS && diffTime < THIRTY_DAYS_IN_MS) {
+    return "active";
+  }
+
+  return "non-active";
 };
 
 export const { POST } = serve<InitialData>(async (context) => {
-  const { email } = context.requestPayload;
+  const { email, fullName } = context.requestPayload;
+
+  // Welcome email
   await context.run("new-signup", async () => {
-    await sendEmail("Welcome to the platform", email);
+    await sendEmail({
+      email,
+      subject: "Welcome to EBBook",
+      message: `Welcome ${fullName} to EBBook. We're excited to have you on board!`,
+    });
   });
 
-  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
+  await context.sleep("wait-for-3-days", THREE_DAYS_IN_MS);
 
   while (true) {
     const state = await context.run("check-user-state", async () => {
-      return await getUserState();
+      return await getUserState(email);
     });
 
     if (state === "non-active") {
       await context.run("send-email-non-active", async () => {
-        await sendEmail("Email to non-active users", email);
+        await sendEmail({
+          email,
+          subject: "Are you still with us?",
+          message: `Hey ${fullName}, we miss you!`,
+        });
       });
     } else if (state === "active") {
       await context.run("send-email-active", async () => {
-        await sendEmail("Send newsletter to active users", email);
+        await sendEmail({
+          email,
+          subject: "Welcome back!",
+          message: `Welcome back ${fullName}! We've missed you!`,
+        });
       });
     }
 
-    await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
+    await context.sleep("wait-for-1-month", THIRTY_DAYS_IN_MS);
   }
 });
-
-async function sendEmail(message: string, email: string) {
-  // Implement email sending logic here
-  console.log(`Sending ${message} email to ${email}`);
-}
-
-type UserState = "non-active" | "active";
-
-const getUserState = async (): Promise<UserState> => {
-  // Implement user state logic here
-  return "non-active";
-};
--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -8,6 +8,8 @@ import { hash } from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { workflowClient } from "../workflow";
+import config from "../config";
 
 export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, password, universityId, universityCard } = params;
@@ -34,6 +36,13 @@ export const signUp = async (params: AuthCredentials) => {
       password: hashedPassword,
       universityId,
       universityCard,
+    });
+
+    await workflowClient.trigger({
+      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      body: {
+        email, fullName
+      },
     });
 
     await signInWithCredentials({ email, password });
